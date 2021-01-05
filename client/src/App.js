3baace4b9853978e106183567e6d1d749ac2cb87
dani7beth <danielledona28@gmail.com>
2021-01-05 15:04:22 -0700

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Demo/Home";
-import Things from "./Demo/Things";
 import NoMatch from "./components/NoMatch";
 import NavBar from "./components/NavBar";
-import Login from "./components/Login";
-import Register from "./components/Register";
 import { Container } from "semantic-ui-react";
 import FetchUser from "./components/FetchUser";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -18,9 +15,6 @@ function App() {
         <Container>
           <Switch>
             <ProtectedRoute exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/things" component={Things} />
             <Route component={NoMatch} />
           </Switch>
         </Container>
