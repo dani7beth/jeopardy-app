4703a224cc2c880a1d395f5d35323633389739c1
dani7beth <danielledona28@gmail.com>
2021-01-07 19:32:05 -0700

    added a create method for category and cards

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Container, Header, Table } from "semantic-ui-react";
 import { CategoryContext } from "../providers/CategoryProvider";
 import { Link } from "react-router-dom";
